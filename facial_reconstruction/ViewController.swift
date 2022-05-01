4e879f805b9c54ca106d77d14450fabfac0ee64d
yarovoi.p.98 <yarovoi.p.98@gmail.com>
2022-05-01 20:50:26 +0700

    Initial Commit

---
+++ facial_reconstruction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  facial_reconstruction
+//
+//  Created by Pavel Yarovoi on 01.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
